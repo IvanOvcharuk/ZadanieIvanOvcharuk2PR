ace3404c1b3d464d74a48add8582e42e70a935ec
IvanOvcharuk <ivanovcharuk228@gmail.com>
2021-11-03 15:30:25 +0100

    Gotowe zadanie

--- app/src/main/java/com/example/zadanieivanovcharuk2pr/MainActivity.kt
+++ app/src/main/java/com/example/zadanieivanovcharuk2pr/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.zadanieivanovcharuk2pr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -17,17 +16,17 @@ class MainActivity : AppCompatActivity() {
         var liczba2 = 0;
         var odpowiedz = 0;
         findViewById<Button>(R.id.zast).setOnClickListener {
-            findViewById<TextView>(R.id.textView).text = findViewById<TextInputEditText>(R.id.wprowadzenie).text
+            findViewById<TextView>(R.id.wyswietlanie).text = findViewById<TextInputEditText>(R.id.wprowadzenie).text
         }
         findViewById<Button>(R.id.dod).setOnClickListener {
-            findViewById<TextView>(R.id.textView).text = findViewById<TextView>(R.id.textView).text.toString() + findViewById<TextInputEditText>(R.id.wprowadzenie).text.toString()
+            findViewById<TextView>(R.id.wyswietlanie).text = findViewById<TextView>(R.id.wyswietlanie).text.toString() + findViewById<TextInputEditText>(R.id.wprowadzenie).text.toString()
         }
         findViewById<Button>(R.id.dodawanie).setOnClickListener {
-            findViewById<TextView>(R.id.textView).text = findViewById<TextView>(R.id.textView).text.toString() + findViewById<TextInputEditText>(R.id.wprowadzenie).text
+            findViewById<TextView>(R.id.wyswietlanie).text = findViewById<TextView>(R.id.wyswietlanie).text.toString() + findViewById<TextInputEditText>(R.id.wprowadzenie).text
             liczba1 = findViewById<EditText>(R.id.Pierwszaliczba).text.toString().toInt()
             liczba2 = findViewById<EditText>(R.id.Drugaliczba).text.toString().toInt()
             odpowiedz = liczba1 + liczba2
-            findViewById<TextView>(R.id.textView).text = odpowiedz.toString()
+            findViewById<TextView>(R.id.wyswietlanie).text = odpowiedz.toString()
 
         }
     }
